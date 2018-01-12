bd39e9735f0a9a6c8f0f5b3f12f3f9c138a679d1
Alok Desai <alok.desai@cuelogic.co.in>
2018-01-12 19:44:25 +0530

    Initial commit

---
+++ Flash Chat/SenderMessageCell.swift
@@ -0,0 +1,22 @@
+//
+//  SenderMessageCell.swift
+//  Flash Chat
+//
+//  Created by Alok Desai on 12/01/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class SenderMessageCell: UITableViewCell {
+
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userMessageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
